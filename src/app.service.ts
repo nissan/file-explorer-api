7d06b5b99aa334abc432a7d0620db0bc75b9669b
Nissan Dookeran <nissan.dookeran@gmail.com>
2020-10-28 23:14:32 +1000

    set geojson filter

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return "Hello friend, you may want to <a href='#'>load the website now</a>";
   }
 }
--- src/utils/file-utils.ts
+++ src/utils/file-utils.ts
@@ -1,7 +1,7 @@
 import { extname } from "path";
 
 export const validFileFilter = (req, file, callback) => {
-    if (!extname(file.originalname).match(/\.(csv|json)$/)) {
+    if (!extname(file.originalname).match(/\.(csv|geojson)$/)) {
       return callback(new Error('Only CSV and GeoJSON files are allowed!'), false);
     }
     callback(null, true);
