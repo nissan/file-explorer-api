53f5811787e2a62f9fcfe13580e51ce85b91c2b6
Nissan Dookeran <nissan.dookeran@gmail.com>
2020-10-28 21:54:00 +1000

    fix typo

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,11 +1,11 @@
 import { Controller, Get, Param, Post, Res, UploadedFile, UploadedFiles, UseInterceptors } from '@nestjs/common';
-import {FileInterceptor, FilesInterceptor} from '@nestjs/platform-express'
+import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express'
 import { FilesService } from './files.service';
 import { diskStorage } from 'multer';
 import { validFileFilter } from 'src/utils/file-utils';
 @Controller('files')
 export class FilesController {
-    constructor(private readonly fileService:FilesService){}
+    constructor(private readonly fileService: FilesService) { }
 
     @Get()
     findAll(): string {
@@ -13,26 +13,26 @@ export class FilesController {
     }
 
     @Post('/upload')
-  @UseInterceptors(
-    FileInterceptor('file', {
-      storage: diskStorage({
-        destination: './files',
-      }),
-      fileFilter: validFileFilter,
-    }),
-  )
-  async uploadedFile(@UploadedFile() file) {
-    console.log(file);
-    const response = {
-      originalname: file.originalname,
-    };
-    return response;
-  }
+    @UseInterceptors(
+        FileInterceptor('file', {
+            storage: diskStorage({
+                destination: './files',
+            }),
+            fileFilter: validFileFilter,
+        }),
+    )
+    async uploadedFile(@UploadedFile() file) {
+        console.log(file);
+        const response = {
+            originalname: file.originalname,
+        };
+        return response;
+    }
 
 
-  @Get('/download/:file')
-  downloadFile(@Param('file') file, @Res() res) {
-    return res.sendFile(file, { root: './files' });
-  }
+    @Get('/download/:file')
+    downloadFile(@Param('file') file, @Res() res) {
+        return res.sendFile(file, { root: './files' });
+    }
 }
 
