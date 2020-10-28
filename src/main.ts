6f2612946030c36e1f562d011bb870449b80c6d4
Nissan Dookeran <nissan.dookeran@gmail.com>
2020-10-28 18:03:38 +1000

    add configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+  await app.listen(configService.get('API_PORT'));
 }
 bootstrap();
