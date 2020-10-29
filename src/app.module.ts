5a74056267a7c65781cf295e7f3d5788284f76ad
Nissan Dookeran <nissan.dookeran@gmail.com>
2020-10-30 05:02:03 +1000

    add db layer but not live yet for crud

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,24 @@ import { MulterModule } from '@nestjs/platform-express';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FilesModule } from './files/files.module';
+import {TypeOrmModule} from '@nestjs/typeorm';
 
 @Module({
-  imports: [FilesModule, MulterModule.registerAsync({
+  imports: [FilesModule,
+    TypeOrmModule.forRoot(
+      {
+      type: 'postgres', // type of our database
+      host: 'localhost', // database host
+      port: 5432, // database host
+      username: 'postgres', // username
+      password: 'pass123', // user password
+      database: 'postgres', // name of our database,
+      autoLoadEntities: true, // models will be loaded automatically (you don't have to explicitly specify the entities: [] array)
+      synchronize: true, // your entities will be synced with the database (ORM will map entity definitions to corresponding SQL tabled), every time you run the application (recommended: disable in the production)
+
+      }
+    ), 
+    MulterModule.registerAsync({
     useFactory: () => ({
       dest: 'uploads/',
     }),})],
--- src/files/files.module.ts
+++ src/files/files.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { FileTypeEntity } from './entities/file-type.entity';
+import { FileEntity } from './entities/file.entity';
 import { FilesController } from './files.controller';
 import { FilesService } from './files.service';
 
 @Module({
   controllers: [FilesController],
   providers: [FilesService],
-  imports :[]
+  imports :[TypeOrmModule.forFeature([FileEntity,FileTypeEntity])]
 })
 export class FilesModule {}
