0febfb193ee19290a34c5c73be999a03da44dd9e
Nissan Dookeran <nissan.dookeran@gmail.com>
2020-10-30 03:50:36 +1000

    update validation to do automatic transforms

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({
-    whitelist:true
+    whitelist:true,
+    forbidNonWhitelisted:true,
+    transform: true
   }));
   const options = new DocumentBuilder()
     .setTitle('File Explorer API')
