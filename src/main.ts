a50ad70a940f3647e6fc06e99ac22222b59f0370
Nissan Dookeran <nissan.dookeran@gmail.com>
2020-10-30 01:59:15 +1000

    add validator

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
@@ -5,7 +6,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.useGlobalPipes(new ValidationPipe());
   const options = new DocumentBuilder()
     .setTitle('File Explorer API')
     .setDescription('The File Explorer API lets you simulate a file system storing files')
