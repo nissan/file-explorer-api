f1a74511ae74a7af373fb2a7f4160a063d807926
Nissan Dookeran <nissan.dookeran@gmail.com>
2020-10-29 22:16:45 +1000

    add file upload rename function, initial file services

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,31 +1,67 @@
-import { Controller, Get, Param, Post, Res, UploadedFile, UploadedFiles, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query, Res, UploadedFile, UploadedFiles, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express'
 import { FilesService } from './files.service';
 import { diskStorage } from 'multer';
-import { validFileFilter } from 'src/utils/file-utils';
+import { editFileName, validFileFilter } from 'src/utils/file-utils';
+import { CreatefileDto } from './dtos/createfile.dto';
 @Controller('files')
 export class FilesController {
     constructor(private readonly fileService: FilesService) { }
 
     @Get()
-    findAll(): string {
-        return 'This action returns all files';
+    findAll(@Query()paginationQuery) {
+        const {limit,offset}=paginationQuery;
+        return this.fileService.findAll(limit, offset);
     }
 
+    @Get(":id")
+    findByFolder(@Param("id")folderId:number){
+        console.log(folderId);
+        return this.fileService.findByFolder(folderId);
+    }
+    @Post()
+    createFolder(@Body() body){
+        return 'This creates a new folder';
+    }
+
+    @Patch(':fileOrFolderid')
+    renameFileOrFilder(@Param('fileOrFolderId')fileOrFolderId:number, 
+                        @Body() body)
+    {
+        return 'Rename a file or folder'
+    }
+
+    @Delete(':fileOrFolderId')
+    deleteFileOrFolder(@Param('fileOrFolderId')fileOrFolderId:number)
+    {
+        return 'Deletes a file or folder'
+    }
+
+// Reference: https://gabrieltanner.org/blog/nestjs-file-uploading-using-multer
     @Post('/upload')
     @UseInterceptors(
         FileInterceptor('file', {
             storage: diskStorage({
                 destination: './files',
+                filename: editFileName,
             }),
             fileFilter: validFileFilter,
         }),
     )
-    async uploadedFile(@UploadedFile() file) {
+    async uploadedFile(@UploadedFile() file, @Body() body) {
         console.log(file);
         const response = {
-            originalname: file.originalname,
+            originalName: file.originalname,
+            fileName: file.filename,
+            filePath: body.filepath,
+            body: JSON.parse(body.createFileDto)
         };
+        let createFileDto:CreatefileDto = body.createFileDto;
+        createFileDto.fileName = file.filename,
+
+
+        this.fileService.createFile(createFileDto)
+
         return response;
     }
 
@@ -34,5 +70,7 @@ export class FilesController {
     downloadFile(@Param('file') file, @Res() res) {
         return res.sendFile(file, { root: './files' });
     }
+
+    
 }
 
--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -1,6 +1,92 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
+import { CreatefileDto } from './dtos/createfile.dto';
+import { FileTypeEntity } from './entities/file-type.entity';
+import { FileEntity } from './entities/file.entity';
+
 
 @Injectable()
 export class FilesService {
-    
+    private fileTypes:FileTypeEntity[] = [
+        {
+            id:1,
+            name:'CSV',
+            extension:'csv'
+        },
+        {
+            id:2,
+            name:'GeoJSON',
+            extension:'geojson'
+        },
+        {
+            id:3,
+            name:'folder',
+            extension:''
+        }
+    ];
+    private files:FileEntity[] = [
+        {
+            id: 1,
+            originalFileName: 'root',
+            storedFileName: '',
+            fileTypeEntityId: 3,
+            parentFolderId: 1,
+            dateCreated: new Date(),
+            dateLastUpdated: new Date(),
+            isDeleted:false
+        },
+        {
+            id: 2,
+            originalFileName: 'documents',
+            storedFileName: '',
+            fileTypeEntityId: 3,
+            parentFolderId: 1,
+            dateCreated: new Date(),
+            dateLastUpdated: new Date(),
+            isDeleted:false
+        },
+        {
+            id: 3,
+            originalFileName: 'sales.csv',
+            storedFileName: 'sales-0123.csv',
+            fileTypeEntityId: 1,
+            parentFolderId: 1,
+            dateCreated: new Date(),
+            dateLastUpdated: new Date(),
+            isDeleted:false
+        },
+        {
+            id: 3,
+            originalFileName: 'sales.csv',
+            storedFileName: 'sales-0001.csv',
+            fileTypeEntityId: 1,
+            parentFolderId: 1,
+            dateCreated: new Date(),
+            dateLastUpdated: new Date(),
+            isDeleted:true
+        },
+        {
+            id: 4,
+            originalFileName: 'salesdistribution.geojson',
+            storedFileName: 'salesdistribution-0113.csv',
+            fileTypeEntityId: 2,
+            parentFolderId: 2,
+            dateCreated: new Date(),
+            dateLastUpdated: new Date(),
+            isDeleted:false
+        },       
+    ]
+
+    findAll(limit:number=20, offset:number=0){
+        return this.files.slice(offset, limit);
+    }
+
+    findByFolder(folderId:number){
+       return this.files.filter(file=>file.id!==+folderId && 
+                        file.parentFolderId===+folderId);
+    }
+
+    createFile(createFileDto:CreatefileDto){
+        //create the information to the database
+        return "done";
+    }
 }
